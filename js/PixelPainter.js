555dce623f74e000caa27ac4d8c246a1d3e464a5
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-16 18:18:20 -1000

    created grid!

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,17 +1,24 @@
 function pixelPainter(){
-let height = 20;
-let width = 30;
+let height = 10;
+let width = 20;
 let module = {};
 let displayDiv = document.getElementById('pixelPainter');
 
 module.createGrid = function(){
   for(x = 0; x < width; x++){
+    let createdRow = document.createElement('div');
     for(y = 0; y < height; y++){
-      let gridBox = document.createElement('button');
-      gridBox.style.color = 'red';
-      displayDiv.appendChild(gridBox);
+      let gridBox = document.createElement('div');
+      gridBox.style.border = 'solid';
+      gridBox.style.border.width = '1px';
+      gridBox.style.height = '10px';
+      gridBox.style.width = '10px';
+      createdRow.appendChild(gridBox);
+      createdRow.className = 'column';
       console.log('hello');
     }
+    displayDiv.appendChild(createdRow);
+
   }
 };
 return module;
