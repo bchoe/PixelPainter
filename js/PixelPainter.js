c79749ad7c5e1fe6dd914c2eab60416ed5da5a14
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-16 17:36:31 -1000

    started coding pixel painter

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,3 +1,13 @@
 function pixelPainter(){
+var height = 20;
+var width = 30;
+
+for(x = 0; x < width; x++){
+  for(y = 0; y < height; y++){
+    var gricBox = document.createElement('div');
+
+  }
+}
+
 
 }
