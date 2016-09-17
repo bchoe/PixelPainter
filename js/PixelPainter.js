090aa05e1e1d388128569a7312209b8247cca392
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-16 17:48:46 -1000

    created buttons to test loop function

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,13 +1,22 @@
 function pixelPainter(){
-var height = 20;
-var width = 30;
+let height = 20;
+let width = 30;
+let module = {};
+let displayDiv = document.getElementById('pixelPainter');
 
-for(x = 0; x < width; x++){
-  for(y = 0; y < height; y++){
-    var gricBox = document.createElement('div');
-
+module.createGrid = function(){
+  for(x = 0; x < width; x++){
+    for(y = 0; y < height; y++){
+      let gridBox = document.createElement('button');
+      gridBox.style.color = 'red';
+      displayDiv.appendChild(gridBox);
+      console.log('hello');
+    }
   }
+};
+return module;
 }
 
+let pPainter = new pixelPainter();
+pPainter.createGrid();
 
-}
